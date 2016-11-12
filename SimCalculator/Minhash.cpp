ed7d775ad4633d7b9e4e613e133f8ccd7d1277b0
gonmolon <gonzalo1796@gmail.com>
2016-11-12 13:42:23 +0100

    Restructuring project

---
+++ SimCalculator/Minhash.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+float computeMinhash(vector<string>& shingles1, vector<string>& shingles2) {
+    return 1;
+}
---
+++ SimCalculator/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include "Jaccard.cpp"
+#include "Minhash.cpp"
+
+using namespace std;
+
+void readFile(std::ifstream& in, std::string& string, int streamsize) {
+    int pos=0;
+    while(pos < streamsize){
+        in.get(string[pos]);
+        ++pos;
+    }
+}
+
+// Params:(name of document, queue for shingles (maybe better other structure), k)
+
+vector<string> file_shingling(string file, int k) {
+    vector<string> shingles;
+    set<string> control;
+    ifstream fIn(file, ios::in);
+    string shingle;
+    shingle.resize(k);
+    int charPos = 1;
+    while(!fIn.eof()) {
+
+        readFile(fIn, shingle, k);
+
+        if(control.find(shingle) == control.end()) {
+            shingles.push_back(shingle);
+            control.insert(shingle);
+        }
+
+        if(fIn.peek() == '\n') ++charPos;
+        if(fIn.eof()) return shingles;
+        fIn.seekg(charPos++);
+
+    }
+    return shingles;
+}
+
+int main() {
+    string file1, file2;
+    cin >> file1 >> file2;
+    int k;
+    cin >> k;
+
+    vector<string> shingles1 = file_shingling(file1, k);
+    vector<string> shingles2 = file_shingling(file2, k);
+
+    cout << "Jaccard similarity: " << computeJaccard(shingles1, shingles2) << endl;
+    cout << "Minhash similarity: " << computeMinhash(shingles1, shingles2) << endl;
+}
+
